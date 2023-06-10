5593e22dd200fc347aaaa1891c9293165a09e8ab
618T <93539314+618T@users.noreply.github.com>
2023-06-10 21:27:53 +0300

    some shit

--- main.cpp
+++ main.cpp
@@ -48,47 +48,66 @@ std::string getZodiacSign(int month, int day) {
     return "";
 }
 
+void printTimeElapsed(const std::chrono::duration<long long>& duration) {
+    auto days = std::chrono::duration_cast<std::chrono::hours>(duration) / 24;
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(duration) % 24;
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration) % 60;
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration) % 60;
+
+    std::cout << "Time Elapsed: " << days.count() << " days, "
+              << hours.count() << " hours, "
+              << minutes.count() << " minutes, "
+              << seconds.count() << " seconds." << std::endl;
+
+    auto years = std::chrono::duration_cast<std::chrono::years>(days);
+    days -= std::chrono::duration_cast<std::chrono::days>(years);
+
+    auto months = std::chrono::duration_cast<std::chrono::months>(days);
+    days -= std::chrono::duration_cast<std::chrono::days>(months);
+
+    std::cout << "Time Elapsed (in day month year): " << days.count() << " days "
+              << months.count() << " months "
+              << years.count() << " years." << std::endl;
+}
+
 int main() {
-    // Get date input from the user
-    std::string date;
-    std::cout << "Enter the date (day month year, For example: 10 06 2000): ";
-    std::getline(std::cin, date);
-
-    // Parse day, month, and year from the input string
+    // Enter the date (day month year)
+    std::cout << "Enter the date (day month year): ";
     int day, month, year;
-    std::istringstream(date) >> day >> month >> year;
+    std::cin >> day >> month >> year;
 
-    // Get current date
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
-    std::tm* now_tm = std::localtime(&now_time);
+    // Get the current date and time
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+    time_t now_time = std::chrono::system_clock::to_time_t(now);
+    struct tm* now_tm = std::localtime(&now_time);
 
-    // Get the target date
-    std::tm target_tm = *now_tm;
-    target_tm.tm_mday = day;
-    target_tm.tm_mon = month - 1;
+    // Set the target date and time
+    struct tm target_tm;
     target_tm.tm_year = year - 1900;
-    std::mktime(&target_tm);
+    target_tm.tm_mon = month - 1;
+    target_tm.tm_mday = day;
+    target_tm.tm_hour = 0;
+    target_tm.tm_min = 0;
+    target_tm.tm_sec = 0;
 
-    // Calculate the difference in days between the two dates
-    std::time_t target_time = std::mktime(&target_tm);
-    double diff_seconds = std::difftime(now_time, target_time);
-    int diff_days = diff_seconds / (60 * 60 * 24);
+    // Calculate the time difference between the current date and the target date
+    time_t target_time = std::mktime(&target_tm);
+    std::chrono::duration<long long> diff_seconds = std::chrono::duration<long long>(now_time - target_time);
 
-    // Calculate the day of the week for the target date
+    // Print the entered date and the current date
+    std::cout << "Entered Date: " << std::setfill('0') << std::setw(2) << day << "." << std::setfill('0') << std::setw(2) << month << "." << year << std::endl;
+    std::cout << "Current Date: " << std::put_time(now_tm, "%d.%m.%Y") << std::endl;
+
+    // Print the day of the week for the target date
     int dayOfWeek = calculateDayOfWeek(day, month, year % 100, year / 100);
     std::string dayName = getDayOfWeek(dayOfWeek);
+    std::cout << "The entered date falls on a " << dayName << "." << std::endl;
+
+    // Print the number of days, hours, minutes, and seconds elapsed
+    printTimeElapsed(diff_seconds);
 
     // Get the zodiac sign for the target date
     std::string zodiacSign = getZodiacSign(month, day);
-
-    // Print the entered date and the current date
-    std::cout << "Current Date: " << std::put_time(now_tm, "%d.%m.%Y") << std::endl;
-    std::cout << "Entered Date: " << std::setfill('0') << std::setw(2) << day << "." << std::setfill('0') << std::setw(2) << month << "." << year << std::endl;
-    
-    std::cout << "The entered date falls on a " << dayName << "." << std::endl;
-    std::cout << "Number of days elapsed since today: " << diff_days << std::endl;
-
     if (!zodiacSign.empty()) {
         std::cout << "Zodiac Sign: " << zodiacSign << std::endl;
     } else {
