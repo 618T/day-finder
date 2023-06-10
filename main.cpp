918fb217d6aa636bd1884dd2d701e9ee76e6c960
618T <93539314+618T@users.noreply.github.com>
2023-06-10 21:06:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 int calculateDayOfWeek(int q, int m, int k, int j) {
     int h;
@@ -14,23 +15,40 @@ std::string getDayOfWeek(int h) {
 }
 
 int main() {
-    int q, m, y;
-
-    std::cout << "Enter the date:\n";
-    std::cout << "Day: ";
-    std::cin >> q;
-    std::cout << "Month: ";
-    std::cin >> m;
+    // Get date input from the user
+    int year, month, day;
     std::cout << "Year: ";
-    std::cin >> y;
+    std::cin >> year;
+    std::cout << "Month: ";
+    std::cin >> month;
+    std::cout << "Day: ";
+    std::cin >> day;
 
-    int k = y % 100; 
-    int j = y / 100; 
+    // Get current date
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+    time_t now_time = std::chrono::system_clock::to_time_t(now);
+    struct tm* now_tm = std::localtime(&now_time);
 
-    int dayOfWeek = calculateDayOfWeek(q, m, k, j);
+    // Get the target date
+    struct tm target_tm;
+    target_tm.tm_year = year - 1900;
+    target_tm.tm_mon = month - 1;
+    target_tm.tm_mday = day;
+    target_tm.tm_hour = 0;
+    target_tm.tm_min = 0;
+    target_tm.tm_sec = 0;
+
+    // Calculate the difference in days between the two dates
+    time_t target_time = std::mktime(&target_tm);
+    double diff_seconds = std::difftime(now_time, target_time);
+    int diff_days = diff_seconds / (60 * 60 * 24);
+
+    // Calculate the day of the week for the target date
+    int dayOfWeek = calculateDayOfWeek(day, month, year % 100, year / 100);
     std::string dayName = getDayOfWeek(dayOfWeek);
 
-    std::cout << "The date entered falls on " << dayName << std::endl;
+    std::cout << "The entered date falls on a " << dayName << "." << std::endl;
+    std::cout << "Number of days elapsed since today: " << diff_days << std::endl;
 
     return 0;
 }
